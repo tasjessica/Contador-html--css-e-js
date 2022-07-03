8fe2c2ffc705c094215307f4c32ccc1ce166bc57
Jessica Andrade <105511258+tasjessica@users.noreply.github.com>
2022-07-02 22:04:45 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,31 @@
 let count = 0;
 
-const CURRENT_NUMBER = document.getElementById('currentNumber');
+const CURRENT_NUMBER = document.getElementById('numero');
 
-function increment() {
+function aumentar() {
 	count++;
 	CURRENT_NUMBER.innerHTML = count;
+	colorirNumero();
 }
 
-function decrement() {
+function diminuir() {
 	count--;
 	CURRENT_NUMBER.innerHTML = count;
+	colorirNumero();
 }
 
 function test() {
 	kdowkdpo;
+}
+
+// menor do que zero vai ficar vermelho e maior ficará verde 
+
+function colorirNumero(){
+	if (count < 0){
+		document.getElementById('numero').style.color="#990000";
+	}else if(count >0){
+		document.getElementById('numero').style.color="#009900";
+	}else{
+		document.getElementById('numero').style.color="black";
+	}
 }
